f51af354bdd3d731b535d81afc5aa565e8fe932b
Ashok <114943948+AshokShau@users.noreply.github.com>
2024-11-21 22:28:06 +0530

    remove mux (#2)

--- main.go
+++ main.go
@@ -6,17 +6,13 @@ import (
 	"net/http"
 
 	"github.com/AshokShau/upi-qr-generator/str"
-	"github.com/gorilla/mux"
 )
 
 func main() {
-	r := mux.NewRouter()
+	http.HandleFunc("/", str.Home)
+	http.HandleFunc("/qr", str.GenerateQRCodeHandler)
+	http.HandleFunc("/pay", str.RenderPaymentPageHandler)
 
-	r.HandleFunc("/", str.Home)
-	r.HandleFunc("/qr", str.GenerateQRCodeHandler).Methods("GET")
-	r.HandleFunc("/pay", str.RenderPaymentPageHandler).Methods("GET")
-	r.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-
-	fmt.Println("Server started at http://localhost:3000")
-	log.Fatal(http.ListenAndServe(":3000", r))
+	fmt.Println("Server started on port 3000")
+	log.Fatal(http.ListenAndServe(":3000", nil))
 }
